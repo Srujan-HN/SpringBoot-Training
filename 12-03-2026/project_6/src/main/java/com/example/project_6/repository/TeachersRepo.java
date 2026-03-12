d33ff9a5d225f8afa81926a8151c5683038efea7
Srujan-HN <srujanhn85@gmail.com>
2026-03-12 23:38:43 +0530

    Delete 12-03-2026/project_6 directory

--- 12-03-2026/project_6/src/main/java/com/example/project_6/repository/TeachersRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.project_6.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.project_6.entity.TeachersEntity;
-
-public interface TeachersRepo extends JpaRepository<TeachersEntity, Integer> {
-    
-}
